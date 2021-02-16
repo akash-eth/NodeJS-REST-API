b4cb2eff383d347691c42cfc996dbde76f3bab72
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-02-16 17:32:51 +0530

    added express

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+
+var server = http.createServer((req, res) => {
+
+    if (req.url === "/") {
+        res.write("Welcome to the page");
+        res.end();
+    }
+
+    if (req.url === "/api/courses") {
+        res.write(JSON.stringify(["DSA", "TOC", "OOPs"]))
+        res.end();
+    }
+
+})
+
+server.listen(3000);
+
+console.log(`
+        Listening at port 3000
+    `);
