5e949170f8c931f61fe55c85ec58acac5d41add0
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-04-29 15:43:56 +0530

    refactored REST API

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+const { json } = require("express");
 const http = require("http");
 
 
+// var server = http.createServer((req, res) => {
+
+//     if (req.url === "/") {
+//         res.write("Welcome to the page");
+//         res.end();
+//     }
+
+//     if (req.url === "/api/courses") {
+//         res.write(JSON.stringify(["DSA", "TOC", "OOPs"]))
+//         res.end();
+//     }
+
+// })
+
+// server.listen(3000);
+
+// console.log(`
+//         Listening at port 3000
+//     `);
+
 var server = http.createServer((req, res) => {
 
-    if (req.url === "/") {
-        res.write("Welcome to the page");
+    if(req.url === "/") {
+        res.write("Ypu are welcome");
         res.end();
     }
 
-    if (req.url === "/api/courses") {
-        res.write(JSON.stringify(["DSA", "TOC", "OOPs"]))
+    if(req.url === "/api/courses") {
+        res.write(JSON.stringify(['DSA', 'TOC', 'CGM']));
         res.end();
     }
 
@@ -17,6 +38,4 @@ var server = http.createServer((req, res) => {
 
 server.listen(3000);
 
-console.log(`
-        Listening at port 3000
-    `);
+console.log('Listening at 3000');
